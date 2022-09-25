54c4c3f74365ac69d26d8a6f46da431f47d91683
tuanngo512 <84236516+tuanngo512@users.noreply.github.com>
2022-09-25 17:20:20 +0700

    Add files via upload

---
+++ MamCo/MamCo/Controllers/CustomerController.cs
@@ -0,0 +1,56 @@
+﻿using MamCo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MamCo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        static List<Customer> customers = new List<Customer>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetbyId(string id)
+        {
+            try
+            {
+                var cus = customers.SingleOrDefault(n => n.id == Guid.Parse(id));
+                if (cus == null)
+                {
+                    return NotFound();
+                }
+                return Ok(cus);
+            } catch
+            {
+                return BadRequest();
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult Create(Customer cus)
+        {
+            var cus1 = new Customer
+            {
+                id = Guid.NewGuid(),
+                Name = cus.Name,
+                Gender = cus.Gender
+            };
+            customers.Add(cus1);
+            return Ok( new
+            {
+                success = true, Data = cus1
+            }
+                );
+        }
+    }
+}
---
+++ MamCo/MamCo/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MamCo.Models
+{
+    public class Customer
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
